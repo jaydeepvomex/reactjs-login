824acab50a5d4fa6548b8e9911b2750e9c1f0b6e
Krunal <p13205072@my365.dmu.ac.uk>
2018-07-05 10:42:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import './App.css';
+import Loginscreen from './LoginScreen';
+
+//injectTapEventPlugin();
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+      uploadScreen:[]
+    }
+  }
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<Loginscreen parentContext={this}/>);
+    this.setState({
+    loginPage:loginPage
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.loginPage}
+        {this.state.uploadScreen}
+      </div>
+    );
+  }
+}
+const style = {
+  margin: 15,
+};
